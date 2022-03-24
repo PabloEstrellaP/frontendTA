84e2a13b7dfa9c9c320b9fd88a72efa68df38b67
victorB56 <80054162+victorB56@users.noreply.github.com>
2022-03-24 09:44:27 -0600

    de

---
+++ components/housingSector/hsCard.vue
@@ -0,0 +1,69 @@
+<template>
+  <v-card class="ma-5">
+      <v-card-title>Inmobilario</v-card-title>
+      <v-card-text>
+         <v-data-table
+              :headers="headers"
+              :items="its"
+              :items-per-page="5"
+            >
+              <template v-slot:[`item.action`]="{ item }">
+                <v-icon
+                  class="mr-2"
+                  @click="setIt(item)"
+                  >mdi-pencil</v-icon
+                >
+                <v-icon
+                  class="mr-2"
+                  @click="openDialog(item)"
+                  >mdi-delete</v-icon
+                >
+              </template>
+            </v-data-table>
+      </v-card-text>
+      <DeleteCardDialog ref="deleteCardDialog"/>
+  </v-card>
+</template>
+
+<script>
+  import DeleteCardDialog from '@/components/it/deleteCardDialog.vue'
+  export default {
+    components: {
+      DeleteCardDialog
+    },
+    data() {
+      return {
+        headers: [
+          { text: "Serie", value: "serial", align: "center" },
+          { text: "Descripción", value: "description", align: "center" },
+          { text: "Responsable", value: "responsableName", align: "center" },
+          { text: "Actions", align: "center", value: "action", sortable: false }
+        ],
+        its: [],
+      }
+    },
+    methods: {
+      async getIt() {
+        try {
+          const data = await this.$axios.$get('/it', {
+            headers: { token: localStorage.getItem('token') }
+          })
+          this.its = data.msg
+        } catch (error) {
+          console.log(error.response)
+        }
+      },
+      openDialog(item) {
+        this.$refs.deleteCardDialog.openDialog(item)
+      },
+      setIt(data) {
+        this.$parent.it = data
+        console.log(this.$parent.it)
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
--- pages/housingSector.vue
+++ pages/housingSector.vue
@@ -1,15 +1,64 @@
 <template>
-  <div>
-      <h1>Hola mundo, soy Inmobiliaria</h1>
-  </div>
+  <v-layout row wrap :class="isLoading ? 'isHiden' : ''">
+    <v-flex xs12 md4>
+      <AddITCard ref="addITCard"/>
+    </v-flex>
+    <v-flex xs12 md8>
+      <ITCard ref="iTCard" />
+    </v-flex>
+    <LoadingCardDialog ref="loadingCardDialog" />
+  </v-layout>
 </template>
 
 <script>
-    export default {
-
+  import ITCard from '@/components/housingSector/hsCard.vue'
+  import AddITCard from '@/components/housingSector/addhs.vue'
+  import LoadingCardDialog from '@/components/helpers/loadingCardDialog.vue'
+  export default {
+    components: {
+      ITCard,
+      AddITCard,
+      LoadingCardDialog
+    },
+    data() {
+      return {
+        it: null,
+        isLoading: true
+      }
+    },
+    methods: {
+      async getIt() {
+        await this.$refs.iTCard.getIt()
+      },
+      openDialog() {
+        this.$refs.loadingCardDialog.openDialog()
+      },
+      closeDialog() {
+        this.$refs.loadingCardDialog.closeDialog()
+      }
+    },
+    async mounted() {
+      if(process.browser){
+        this.openDialog()
+        await this.getIt()
+        this.closeDialog()
+        this.isLoading = false
+      }
+    },
+    watch: {
+      it(newvalue) {
+        if(newvalue){
+          this.$refs.addITCard.getIt(newvalue)
+        }else {
+          this.$refs.addITCard.$refs.form.reset()
+        }
+      }
     }
+  }
 </script>
 
-<style>
-
+<style scoped>
+  .isHiden {
+    visibility: hidden;
+  }
 </style>
